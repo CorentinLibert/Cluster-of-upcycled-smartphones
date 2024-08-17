0a7df0469c10eae4c932588f5e9a1c4da6eb4690
CorentinLibert <corentin@libert.be>
2024-08-18 01:47:19 +0200

    [CLUSTER BENCHMARK] Complete experiments with results and graphs

--- Benchmark/homemade/cluster_benchmark/scripts/network_usage_script.sh
+++ Benchmark/homemade/cluster_benchmark/scripts/network_usage_script.sh
@@ -5,6 +5,7 @@ usage() {
     echo "  COUNT:          The number of measurements that should be taken (at an interval of 1 second)."
     echo "  FILENAME:       The path to the result file."
     echo "  INTERFACE:      The interface to monitor."
+    echo "  DISTRIBUTION:   Alpine or Ubuntu distribution."
     echo "  RESET:          1 if results should overwrite existing data, otherwise results are appended."
 }
 
@@ -16,7 +17,8 @@ help() {
 COUNT=$1
 FILENAME=$2
 INTERFACE=$3
-RESET=$4
+DISTRIBUTION=$4
+RESET=$5
 
 # Display help and usage
 if [ $1 == "-h" ] || [ $1 == "--help" ]; then 
@@ -25,7 +27,7 @@ if [ $1 == "-h" ] || [ $1 == "--help" ]; then
 fi
 
 # Check number of args
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
     echo "[Error] Wrong number of arguments: $#."
     usage
     exit 1
@@ -34,13 +36,18 @@ fi
 # Create result file and parent directory if needed
 if [ ! -f $FILENAME ] || [ $RESET -eq 1 ]; then
     mkdir -p "$(dirname $FILENAME)"
-    echo "rx_bytes,tx_bytes" > $FILENAME
+    echo "timestamp,rx_bytes,tx_bytes" > $FILENAME
 fi
 
 # Measure
 for i in $(seq 1 $COUNT); do
     timestamp=$(date +%s)
-    net_info="$(ifconfig $INTERFACE | grep -E "RX bytes|TX bytes" | awk '{print $2, $6}' |  sed 's/bytes://g' | sed 's/ /,/g')"
+    net_info=""
+    if [ "$DISTRIBUTION" = "Alpine" ]; then
+        net_info="$(ifconfig $INTERFACE | grep -E "RX bytes|TX bytes" | awk '{print $2, $6}' |  sed 's/bytes://g' | sed 's/ /,/g')"
+    else
+        net_info="$(/sbin/ifconfig $INTERFACE | grep -E "RX packets|TX packets" | awk '{print $5}' | paste -sd ',')"
+    fi
     echo $timestamp","$net_info >> $FILENAME
     sleep 1
 done
